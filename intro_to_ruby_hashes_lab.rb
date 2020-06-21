123f9a8ce7040bf9106628512dddcc3966d5cd0b
Jessica Pereira <jcpereira070@gmail.com>
2020-06-21 04:55:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,10 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-    railroads: {
+  monopoly = {railroads: {
       pieces: 4
+      names: {}
+      rent_in_dollars: {}
     }
   }
 end
